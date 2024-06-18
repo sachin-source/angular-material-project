45bb8bce2d526f3103480a08c06f947c69670359
sachin-source <sachinmgvt@gmail.com>
2024-06-18 11:05:21 +0530

    added Dialog component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+import {
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, EmpAddEditComponent,],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'angular-new-project';
+  readonly dialog = inject(MatDialog);
+
+  openAddEditEmpForm(){
+    this.dialog.open(EmpAddEditComponent)
+  }
+
 }
