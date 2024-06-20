b86b918e221c7b37e2b1e0c929101a05a664899d
sachin-source <sachinmgvt@gmail.com>
2024-06-20 09:28:35 +0530

    added employee add method to http service

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
+  http = inject(HttpClient);
   constructor() { }
+  addEmployee(data:any) : Observable<any> {
+    return this.http.post('http://localhost:3000/employees', data)
+  }
 }
