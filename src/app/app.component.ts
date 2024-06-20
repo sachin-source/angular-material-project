e9156909a7442d042703e6d0891c7fa10c34face
sachin-source <sachinmgvt@gmail.com>
2024-06-20 19:43:22 +0530

    employee table UI with functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,105 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, ViewChild, inject } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
 import { MatDialog } from '@angular/material/dialog';
 import { EmployeeService } from './services/employee.service';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatSort, MatSortModule} from '@angular/material/sort';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { CurrencyPipe, DatePipe } from '@angular/common';
+
+export interface UserData {
+  id: string;
+  name: string;
+  progress: string;
+  fruit: string;
+}
+
+/** Constants used to fill up our data base. */
+const FRUITS: string[] = [
+  'blueberry',
+  'lychee',
+  'kiwi',
+  'mango',
+  'peach',
+  'lime',
+  'pomegranate',
+  'pineapple',
+];
+const NAMES: string[] = [
+  'Maia',
+  'Asher',
+  'Olivia',
+  'Atticus',
+  'Amelia',
+  'Jack',
+  'Charlotte',
+  'Theodore',
+  'Isla',
+  'Oliver',
+  'Isabella',
+  'Jasper',
+  'Cora',
+  'Levi',
+  'Violet',
+  'Arthur',
+  'Mia',
+  'Thomas',
+  'Elizabeth',
+];
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, EmpAddEditComponent],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, EmpAddEditComponent, MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule, DatePipe, CurrencyPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   // Injections
   readonly dialog = inject(MatDialog);
   employeeService = inject(EmployeeService)
 
-  readonly data$ = this.employeeService.getEmployeeList();
+  // readonly data$ = this.employeeService.getEmployeeList();
 
   openAddEditEmpForm(){
     this.dialog.open(EmpAddEditComponent)
   }
 
-  // getEmployeeList() {
-  //   this.employeeService.getEmployeeList().subscribe({
-  //     next : console.log,
-  //     error: console.log
-  //   })
-  // }
+  displayedColumns: string[] = ['id', 'firstName', 'lastName', 'email', 'dob', 'gender', 'education', 'company', 'experience', 'package', 'action'];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.getEmployeeList()
+  }
+  getEmployeeList() {
+    this.employeeService.getEmployeeList().subscribe({
+      next: (res:any) => {
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      }, 
+      error : console.log
+    })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
