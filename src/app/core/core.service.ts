a6862852fac702e9dcfbf6da6ae03f85870e2fb8
sachin-source <sachinmgvt@gmail.com>
2024-06-21 19:53:35 +0530

    snackbar handling through service

--- src/app/core/core.service.ts
+++ src/app/core/core.service.ts
@@ -1,9 +1,27 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import {
+  MatSnackBar,
+  MatSnackBarAction,
+  MatSnackBarActions,
+  MatSnackBarLabel,
+  MatSnackBarRef,
+} from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoreService {
 
+  _snackBar = inject(MatSnackBar);
+
   constructor() { }
+
+  openSnackBar(message:string, action:string) {
+    this._snackBar.open(message, action,
+      
+      {
+      duration: 1000,
+      verticalPosition: 'top'
+    });
+  }
 }
